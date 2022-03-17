67da671ae271073787af11d263e006aadc8b9ec1
Will <wbrubaker@gmail.com>
2022-03-17 09:37:36 -1000

    re-refactored :)

--- src/Addon.jsx
+++ src/Addon.jsx
@@ -171,155 +171,184 @@ export default class Boilerplate extends Component {
 
 	weekContent(week) {
 		if (2 === week) {
-			const content = () =>
-				(
-					<div>
-						<Card
-							header={<Title>Day 1</Title>}
-							content={
-								<div>
-									<p>
-										Today you will be installing WooCommerce
-										and demo content and becoming familiar
-										with the settings.
-									</p>
-								</div>
-							}
-							footer={
-								<div>
-									<ul style={{ listStyle: "none" }} class="wizard-hat">
-								<li>
-									<Button className="woo button">Install WooCommerce</Button>
+			const content = () => (
+				<div>
+					<Card
+						header={<Title>Day 1</Title>}
+						content={
+							<div>
+								<p>
+									Today you will be installing WooCommerce and
+									demo content and becoming familiar with the
+									settings.
+								</p>
+							</div>
+						}
+						footer={
+							<div>
+								<ul
+									style={{ listStyle: "none" }}
+									class="wizard-hat"
+								>
+									<li>
+										<Button className="woo button">
+											Install WooCommerce
+										</Button>
 									</li>
 									<li>
-									<Button className="woo button">Install Demo Content</Button>
+										<Button className="woo button">
+											Install Demo Content
+										</Button>
 									</li>
-									</ul>
-								</div>
-							}
-						/>
-					</div>
-				);
+								</ul>
+							</div>
+						}
+					/>
+				</div>
+			);
 			return content;
 		}
 		return null;
 	}
 
+	storeConfig = () => (
+		<ul style={{ listStyle: "none" }} class="wizard-hat">
+			<li>
+				<Button
+					onClick={this.switchCountry.bind(this, "United States", {
+						woocommerce_store_address: "537 Paper Street",
+						woocommerce_store_address_2: "#34",
+						woocommerce_store_city: "Wilmington",
+						woocommerce_default_country: "US:DE",
+						woocommerce_store_postcode: "19806",
+						woocommerce_currency: "USD",
+						woocommerce_price_thousand_sep: ",",
+						woocommerce_price_decimal_sep: ".",
+						woocommerce_weight_unit: "lbs",
+						woocommerce_dimension_unit: "in",
+					})}
+					className="woo button"
+				>
+					Switch Site to United States
+					{this.renderSpinner()}
+				</Button>
+			</li>
+			<li>
+				<Button
+					onClick={this.switchCountry.bind(this, "Europe", {
+						woocommerce_store_address: "Brederopad 77",
+						woocommerce_store_address_2: "",
+						woocommerce_store_city: "Delft",
+						woocommerce_default_country: "NL",
+						woocommerce_store_postcode: "2624 XR",
+						woocommerce_currency: "EUR",
+						woocommerce_price_thousand_sep: " ",
+						woocommerce_price_decimal_sep: ",",
+						woocommerce_weight_unit: "kg",
+						woocommerce_dimension_unit: "cm",
+					})}
+					className="woo button"
+				>
+					Switch Site to Europe
+					{this.renderSpinner()}
+				</Button>
+			</li>
+			<li>
+				<Button
+					onClick={this.switchCountry.bind(this, "Australia", {
+						woocommerce_store_address: "28 Kaesler Road",
+						woocommerce_store_address_2: "",
+						woocommerce_store_city: "Mount Burr",
+						woocommerce_default_country: "AU:SA",
+						woocommerce_store_postcode: "5279",
+						woocommerce_currency: "AUD",
+						woocommerce_price_thousand_sep: " ",
+						woocommerce_price_decimal_sep: ",",
+						woocommerce_weight_unit: "kg",
+						woocommerce_dimension_unit: "cm",
+						woocommerce_dimension_unit: "in",
+					})}
+					className="woo button"
+				>
+					Switch Site to Australia
+					{this.renderSpinner()}
+				</Button>
+			</li>
+			<li>
+				<Button
+					onClick={this.switchCountry.bind(this, "Canada", {
+						woocommerce_store_address: "40 Bay St",
+						woocommerce_store_address_2: "",
+						woocommerce_store_city: "Toronto",
+						woocommerce_default_country: "CA:ON",
+						woocommerce_store_postcode: "M5J 2X2",
+						woocommerce_currency: "CAD",
+						woocommerce_price_thousand_sep: " ",
+						woocommerce_price_decimal_sep: ",",
+						woocommerce_weight_unit: "kg",
+						woocommerce_dimension_unit: "cm",
+					})}
+					className="woo button"
+				>
+					Switch Site to Canada
+					{this.renderSpinner()}
+				</Button>
+			</li>
+		</ul>
+	);
+
+	Tools = () => (
+		<ul style={{ listStyle: "none" }} class="wizard-hat">
+			<li>
+				<Button onClick={this.launchPostman} className="woo button">
+					Launch Postman
+				</Button>
+			</li>
+		</ul>
+	);
+
+	Excercises = () => (
+		<ul style={{ listStyle: "none" }} class="wizard-hat">
+			<li>
+				<Button onClick={this.testRequest} className="woo button">
+					Test d/l install plugin
+				</Button>
+			</li>
+		</ul>
+	);
+
+	tokenInput = () => (
+		<div
+			style={{
+				flexGrow: "1",
+				overflow: "auto",
+				position: "relative",
+			}}
+			class="woo gh-token"
+		>
+			<Card>
+				<p>
+					The token is invalid. You will need a valid{" "}
+					<a href="https://github.com/settings/tokens">
+						GitHub token
+					</a>{" "}
+					with 'repo' scope enabled.
+				</p>
+				<p>Please enter a valid token to continue.</p>
+				<InputPasswordToggle
+					onChange={(event) =>
+						this.maybeSaveToken(event.target.value)
+					}
+					onBlur={(event) => this.maybeSaveToken(event.target.value)}
+				/>
+			</Card>
+		</div>
+	);
+
 	render() {
-		const storeConfig = () => (
-			<ul style={{ listStyle: "none" }} class="wizard-hat">
-				<li>
-					<Button
-						onClick={this.switchCountry.bind(
-							this,
-							"United States",
-							{
-								woocommerce_store_address: "537 Paper Street",
-								woocommerce_store_address_2: "#34",
-								woocommerce_store_city: "Wilmington",
-								woocommerce_default_country: "US:DE",
-								woocommerce_store_postcode: "19806",
-								woocommerce_currency: "USD",
-								woocommerce_price_thousand_sep: ",",
-								woocommerce_price_decimal_sep: ".",
-								woocommerce_weight_unit: "lbs",
-								woocommerce_dimension_unit: "in",
-							}
-						)}
-						className="woo button"
-					>
-						Switch Site to United States
-						{this.renderSpinner()}
-					</Button>
-				</li>
-				<li>
-					<Button
-						onClick={this.switchCountry.bind(this, "Europe", {
-							woocommerce_store_address: "Brederopad 77",
-							woocommerce_store_address_2: "",
-							woocommerce_store_city: "Delft",
-							woocommerce_default_country: "NL",
-							woocommerce_store_postcode: "2624 XR",
-							woocommerce_currency: "EUR",
-							woocommerce_price_thousand_sep: " ",
-							woocommerce_price_decimal_sep: ",",
-							woocommerce_weight_unit: "kg",
-							woocommerce_dimension_unit: "cm",
-						})}
-						className="woo button"
-					>
-						Switch Site to Europe
-						{this.renderSpinner()}
-					</Button>
-				</li>
-				<li>
-					<Button
-						onClick={this.switchCountry.bind(this, "Australia", {
-							woocommerce_store_address: "28 Kaesler Road",
-							woocommerce_store_address_2: "",
-							woocommerce_store_city: "Mount Burr",
-							woocommerce_default_country: "AU:SA",
-							woocommerce_store_postcode: "5279",
-							woocommerce_currency: "AUD",
-							woocommerce_price_thousand_sep: " ",
-							woocommerce_price_decimal_sep: ",",
-							woocommerce_weight_unit: "kg",
-							woocommerce_dimension_unit: "cm",
-							woocommerce_dimension_unit: "in",
-						})}
-						className="woo button"
-					>
-						Switch Site to Australia
-						{this.renderSpinner()}
-					</Button>
-				</li>
-				<li>
-					<Button
-						onClick={this.switchCountry.bind(this, "Canada", {
-							woocommerce_store_address: "40 Bay St",
-							woocommerce_store_address_2: "",
-							woocommerce_store_city: "Toronto",
-							woocommerce_default_country: "CA:ON",
-							woocommerce_store_postcode: "M5J 2X2",
-							woocommerce_currency: "CAD",
-							woocommerce_price_thousand_sep: " ",
-							woocommerce_price_decimal_sep: ",",
-							woocommerce_weight_unit: "kg",
-							woocommerce_dimension_unit: "cm",
-						})}
-						className="woo button"
-					>
-						Switch Site to Canada
-						{this.renderSpinner()}
-					</Button>
-				</li>
-			</ul>
-		);
-
-		const Tools = () => (
-			<ul style={{ listStyle: "none" }} class="wizard-hat">
-				<li>
-					<Button onClick={this.launchPostman} className="woo button">
-						Launch Postman
-					</Button>
-				</li>
-			</ul>
-		);
-
-		const Excercises = () => (
-			<ul style={{ listStyle: "none" }} class="wizard-hat">
-				<li>
-					<Button onClick={this.testRequest} className="woo button">
-						Test d/l install plugin
-					</Button>
-				</li>
-			</ul>
-		);
-
 		if (
 			"running" ===
-				this.props.siteStatuses[this.props.match.params.siteID] &&
-			this.state.tokenIsValid
+			this.props.siteStatuses[this.props.match.params.siteID]
 		) {
 			return (
 				<div style={{ flex: "1", overflowY: "auto", margin: "10px" }}>
@@ -328,17 +357,32 @@ export default class Boilerplate extends Component {
 					<div id="wootertiarynav">
 						<TertiaryNav>
 							<TertiaryNavItem
-								path="/item1"
-								component={storeConfig}
+								path="/shop-config"
+								component={
+									this.state.tokenIsValid
+										? this.storeConfig
+										: this.tokenInput
+								}
 							>
-								Store Config Options
+								Shop Config Options
 							</TertiaryNavItem>
-							<TertiaryNavItem path="/item2" component={Tools}>
+							<TertiaryNavItem
+								path="/tools"
+								component={
+									this.state.tokenIsValid
+										? this.Tools
+										: this.tokenInput
+								}
+							>
 								Tools
 							</TertiaryNavItem>
 							<TertiaryNavItem
-								path="/item3"
-								component={Excercises}
+								path="/excercises"
+								component={
+									this.state.tokenIsValid
+										? this.Excercises
+										: this.tokenInput
+								}
 							>
 								Excercises
 							</TertiaryNavItem>
@@ -348,45 +392,16 @@ export default class Boilerplate extends Component {
 							</Title>
 							<TertiaryNavItem
 								path="/week2"
-								component={this.weekContent(2)}
+								component={
+									this.state.tokenIsValid
+										? this.weekContent(2)
+										: this.tokenInput
+								}
 							>
 								Week 2
 							</TertiaryNavItem>
 						</TertiaryNav>
 					</div>
-				</div>
-			);
-		} else if (
-			"running" ===
-			this.props.siteStatuses[this.props.match.params.siteID]
-		) {
-			return (
-				<div
-					style={{
-						flexGrow: "1",
-						overflow: "auto",
-						position: "relative",
-					}}
-					class="woo gh-token"
-				>
-					<Card>
-						<p>
-							The token is invalid. You will need a valid{" "}
-							<a href="https://github.com/settings/tokens">
-								GitHub token
-							</a>{" "}
-							with 'repo' scope enabled.
-						</p>
-						<p>Please enter a valid token to continue.</p>
-						<InputPasswordToggle
-							onChange={(event) =>
-								this.maybeSaveToken(event.target.value)
-							}
-							onBlur={(event) =>
-								this.maybeSaveToken(event.target.value)
-							}
-						/>
-					</Card>
 				</div>
 			);
 		} else {
